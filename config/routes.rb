f18f1ab25d679dec4a3619551f97f97863898273
Mark Muller <mark.muller.w@gmail.com>
2023-05-24 15:21:20 +0200

    added the routes paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # List all restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # Add a new restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # Show details of a restaurant with its reviews
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # Add a new review to a restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: 'new_review'
+  post 'restaurants/:id/reviews', to: 'reviews#create', as: 'restaurant_reviews'
 end
