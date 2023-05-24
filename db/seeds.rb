d700f6d33224cd06de9deb22f494db10cb843f17
Mark Muller <mark.muller.w@gmail.com>
2023-05-24 15:12:53 +0200

    gemfile faker and seeded 10 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Clear existing records
+Review.delete_all
+Restaurant.delete_all
+
+# Create restaurants
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::CATEGORIES.sample
+  )
+
+  if restaurant.save
+    # Create reviews for each restaurant
+    3.times do
+      review = Review.new(
+        content: Faker::Lorem.paragraph(sentence_count: 2),
+        rating: rand(0..5),
+        restaurant: restaurant
+      )
+      review.save
+    end
+  end
+end
